9a0cd24c2ed4d08b678288a40d6a1538838f4a97
harikamat <harikamat@hotmail.com>
2018-11-13 10:49:06 +0530

    basket service interface implemented

---
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using MyShop.Core.ViewModels;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddtoBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string productId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModelcs GetBasketSummary(HttpContextBase httpContext);
+    }
+}
---
+++ MyShop/MyShop.Core/ViewModels/BasketSummaryViewModelcs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Core.ViewModels
+{
+    public class BasketSummaryViewModelcs
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+
+        public BasketSummaryViewModelcs()
+        {
+        }
+
+        public BasketSummaryViewModelcs(int basketCount, decimal basketTotal)
+        {
+            this.BasketCount = basketCount;
+            this.BasketTotal = basketTotal;
+        }
+
+        
+    }
+}
